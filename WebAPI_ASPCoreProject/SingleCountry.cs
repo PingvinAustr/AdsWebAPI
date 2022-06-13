d618671e2e22f4a75d5b0be45fd841f213d6c74d
Yaroslav <yarik.chushenko1@gmail.com>
2022-06-13 16:47:27 +0300

    Single country to get from InputData.json

---
+++ WebAPI_ASPCoreProject/SingleCountry.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI_ASPCoreProject
+{
+    public class SingleCountry
+    {
+        public string SingleCountryName { get; set; }
+        public string[] AvailableVerticals { get; set; }
+        public string[] AvailableRegions { get; set; }
+        public int SingleCountryClicks { get; set; }
+
+        public string SingleCountryLogo { get; set; }
+
+    }
+}
