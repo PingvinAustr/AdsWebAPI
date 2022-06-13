475da98cb5deca34dbfcaaad7142bca767b2efb2
Yaroslav <yarik.chushenko1@gmail.com>
2022-06-13 16:48:07 +0300

    Data to return from API

---
+++ WebAPI_ASPCoreProject/CountryClicks.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAPI_ASPCoreProject
+{
+    public class CountryClicks
+    {
+       public CountryClicks()
+        {
+            
+        }
+       public List<string>? SuitableCountriesArr { get; set; }
+
+       public List<string>?  IMG_LinkArr { get; set; }
+
+       public int SumClicks { get; set; }
+
+
+    }
+}
