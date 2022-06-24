8f6ecdfd20378fde962fe2a1d3cff3a36af34cc9
Yaroslav <yarik.chushenko1@gmail.com>
2022-06-24 12:56:28 +0300

    Minor refactoring changes

--- WebAPI_ASPCoreProject/wwwroot/js/script.js
+++ WebAPI_ASPCoreProject/wwwroot/js/script.js
@@ -1,11 +1,24 @@
-﻿var selectedChildID_DD1 = 0;
+﻿//For dropdown elements on the 1st part of the form. Index of chosen elements
+var selectedChildID_DD1 = 0;
 var selectedChildID_DD2 = 0;
-let inputs_validation = ['False', 'False','False'];
+
+//Represents 3 input fields on 1st part of the form. If the field is filled - True, otherwise - False
+let inputs_validation = ['False', 'False', 'False'];
+
+//Pretty output span template
 var clicks_span_html = "<span id='clicks_span'> clicks</span>";
+//Global variables for chosen region and vertical
 var g_vertical, g_region;
 
+
+
+
+
+//Function that calls API, processes data and outputs that on the page
 function CallClicksAPI(t_vertical, t_region, t_budget) {
 
+
+    //_____Creating request______
     var requestURL = 'https://localhost:7291/ClicksAPI/?vertical=' + t_vertical + '&region=' + t_region;
     if (t_budget != null) requestURL += '&budget=' + t_budget;
 
@@ -13,35 +26,41 @@ function CallClicksAPI(t_vertical, t_region, t_budget) {
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
+    //___________________________
 
-    //alert(document.getElementById('clicks_output').innerHTML);
+
+    //Save current displayed amount of clicks (for animation)
     let current_number_clicks = String(document.getElementById('clicks_output').innerHTML);
     current_number_clicks = parseInt(current_number_clicks.substring(0, current_number_clicks.indexOf('<')));
-    //alert('{' + current_number_clicks + '}');
+   
 
+    //Processing response
     request.onload = function () {
-        var superHeroes = request.response;
        
         console.log(request.response);
+
+        //Display new amount of clicks
         document.getElementById('clicks_output').innerHTML = request.response['sumClicks'] + clicks_span_html;
+
+        //Display images from API to html grid
         LoadImagesToGrid(request.response, request.response['imG_LinkArr'].length);
 
+        //Show/hide 'more...' block depending on number of elements from API response (>6 - show, <=6 - do not show)
         if (request.response['imG_LinkArr'].length <= 6) document.getElementById('more_images').classList.add('more_img_disabled');
 
+        //Play animation of clicks FROM current clicks number to NEW clicks number 
         animateValue('clicks_output', current_number_clicks, request.response['sumClicks'], 1000);
        
     }
-   
-    
-    //document.getElementById('clicks_output').innerHTML = request.response['sumClicks'];
 }
 
 
-
+//Function that gets all images from API response and displays them to html grid
 function LoadImagesToGrid(resp,arr_length) {
 
     let i = 1;
-    
+
+    //Go through each API response element and create html <img/> using its image path
     resp['imG_LinkArr'].forEach(element => {
         if (i <= 6) {
             let text_id = "grid_" + i;
@@ -49,54 +68,54 @@ function LoadImagesToGrid(resp,arr_length) {
         }
         i++;
     });
-
-    /*
-    for (let i = 1; i < arr_length+1; i++) {
-        let text_id = "grid_" + i;
-        let src_text = resp['imG_LinkArr'];
-        alert(src_text);
-        document.getElementById(text_id).innerHTML = "<img src=''/>";
-    }
-  */
 }
 
+
+//Function that is triggered after user has finished 1st form part and clicked 'reveal'
 function RevealButtonClick() {
+
+    //Save chosen vertical and region
     let vertical = document.getElementById("dropdown_placeholder_1").innerHTML;
     let region = document.getElementById("dropdown_placeholder_2").innerHTML;
 
-
+    //Remove all '1st part' html elements from the form 
     var items_to_del = document.querySelectorAll('.form_part');
     items_to_del.forEach(item => {
         item.remove();
     });
 
-   
+    //Create new html elements ('2st part') and insert them into the form
     let toggleHTML = "<div class='toggle_body'><div class='toggle_line'><div class='toggle_ball' id='toggle_ball' onmousedown='dragElement()'></div></div></div>";
     let gridHTML = "<div class='grid_countries'> <div id='grid_1'></div><div id='grid_2'></div><div id='grid_3'></div><div id='grid_4'></div><div id='grid_5'></div><div id='grid_6'></div> </div>";
-     //let test = "CallClicksAPI('" + vertical + "','" + region + "','" + document.getElementById('mobile_input').innerHTML + "')";
     let inputHTML = "<input class='form_input' id='mobile_input' autocomplete='off' type='text'/>";
     document.getElementById('form_body').innerHTML = "<h2 class='form_part'>This is what you can get with MegatronQ:</h2> <h2 class='form_part' style='text-align:center; font-size:48px;' id='clicks_output'>1" + clicks_span_html + "</h2><div>*Your montly budget:<div id='change_budget' style='text-align:center; margin-top:10px; font-weight:bold;'>5000$</div></div>" + toggleHTML + inputHTML+"<div class='JSadded'>Your content will be featured on the <span>World’s Leading Publishers</span></div>" + gridHTML + "<div id='more_images'>and more...</div>";
     document.getElementById('form_body').classList.add('JS');
 
-   // document.getElementById('mobile_input').addEventListener('input', CallClicksAPI(vertical, region, 1000));
+
+    //Adding API handler to the input element that is displayed instead of toggle (mobile only)
     document.getElementById('mobile_input').oninput = function () {
-        //alert(document.getElementById('mobile_input').value);
+
+
         if (document.getElementById('mobile_input').value > 100000) {
             alert('Max budget is 100000$!');
             document.getElementById('mobile_input').value = 100000;
+        } 
+        else {
+            let str = String(document.getElementById('mobile_input').value);
+            let allnums = "0123456789";
+
+            if ((allnums.indexOf(str[str.length - 1])) == -1 && document.getElementById('mobile_input').value != "") { document.getElementById('mobile_input').value = 5000; alert("Please enter only numbers!");}
+            else
+            CallClicksAPI(vertical, region, document.getElementById('mobile_input').value);
         }
-        else
-        CallClicksAPI(vertical, region, document.getElementById('mobile_input').value);
     }
-    //alert(document.getElementById('mobile_input').oninput);
 
-
+    //Save chosen region and vertical and call api based on user choice
     g_vertical = vertical;
     g_region = region;
     CallClicksAPI(vertical, region);
-
     
-    // Make the DIV element draggable:
+    // Make the 'toggle' DIV element draggable:
     dragElement(document.getElementById("toggle_ball"));
 }
 
@@ -108,28 +127,39 @@ function RevealButtonClick() {
 
 
 
-
+//Function to enable/disable 'reveal' button based on number of filled fields
 function ValidateFields() {
     let counter = 0;
+
+   
     for (let i = 0; i < 3; i++) {
         if (inputs_validation[i] == 'True') {
             counter++;
         }
     }
-    
+
+     //If all 3 fields are filled - 'reveal' button is enabled
     if (counter == 3) {
         document.getElementById('submit_button').classList.remove('disabled');
+        document.getElementById('submit_button').onclick = function(){
+            RevealButtonClick();
+        }
     }
+
+    //...Otherwise - disabled
     else {
         document.getElementById('submit_button').classList.add('disabled');
+        document.getElementById('submit_button').onclick = "";
     }
 }
 
+
+//Validation of the first input (should allow only website addresses)
 function onTopInputChange() {
 
     if (document.getElementById('top_input_enter').value.length != 0) {
         
-       
+        //Using Regex. If field is empty / regex validation failed - inputs_validation[0]=False, corresponding CSS error class added.
         let regex_check = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/.test(document.getElementById('top_input_enter').value);
         if (regex_check) {
             inputs_validation[0] = 'True';
@@ -149,13 +179,15 @@ function onTopInputChange() {
         document.getElementById('top_input_enter').classList.add('input_fail');
        
     }
+
+    //Validate fields to enable/disable 'reveal' button
     ValidateFields();
 }
 
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction(x) {
+function ToggleDropdownFunction(x) {
     if (x == '0') {
         document.getElementById("myDropdown").classList.toggle("show");
 
@@ -242,7 +274,6 @@ function dragElement(elmnt) {
         e.preventDefault();
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
-        //pos4 = e.clientY;
         document.onmouseup = closeDragElement;
         // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
@@ -251,49 +282,44 @@ function dragElement(elmnt) {
     function elementDrag(e) {
         e = e || window.event;
         e.preventDefault();
-        //alert(e.clientX);
         // calculate the new cursor position:
         pos1 = pos3 - e.clientX;
-        //pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
-        //pos4 = e.clientY;
-        // set the element's new position:
-        //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        
-
-        //alert(window.innerWidth + " | LineLength:" + window.innerWidth * 0.5 * 0.75 * 0.65 + " | LeftBorderPX:" + window.innerWidth * 0.25 + window.innerWidth * 0.5 * 0.125 + window.innerWidth * 0.5*0.75*0.175);
-
 
         if ((elmnt.offsetLeft - pos1) >= Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 * 0.125 + window.innerWidth * 0.5 * 0.75 * 0.175-7.5) && (elmnt.offsetLeft - pos1) <= Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 - window.innerWidth * 0.5 * 0.125 - window.innerWidth * 0.5 * 0.75 * 0.175-22.5))
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        //alert(elmnt.offsetLeft);
+     
     }
 
     function closeDragElement() {
         let current_pos = String(elmnt.style.left);
         current_pos = parseInt(current_pos.substring(0, current_pos.length - 2));
-       
-        //alert(current_pos);
+
+        //Count left and right borders of toggle line in pixels
         let left = Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 * 0.125 + window.innerWidth * 0.5 * 0.75 * 0.175 - 7.5);
         let right = Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 - window.innerWidth * 0.5 * 0.125 - window.innerWidth * 0.5 * 0.75 * 0.175 - 22.5);
         //alert("Left:" + (Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 * 0.125 + window.innerWidth * 0.5 * 0.75 * 0.175-7.5)));
         //alert("Right:" + (Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 - window.innerWidth * 0.5 * 0.125 - window.innerWidth * 0.5 * 0.75 * 0.175-22.5)));
 
+        //Count length of the toggle line
         let line_length = Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 - window.innerWidth * 0.5 * 0.125 - window.innerWidth * 0.5 * 0.75 * 0.175 - 22.5) - Math.round(window.innerWidth * 0.25 + window.innerWidth * 0.5 * 0.125 + window.innerWidth * 0.5 * 0.75 * 0.175 - 7.5);
+
+        //Split line length in 20 parts
         let one_part_length = line_length / 20;
         let arr_with_borders = [];
         let sum_arr = [];
         let len = left;
         let deb = "";
         sum_arr[0] = 5000;
+
+        //Check where toggle_ball is located and add corresponding number of clicks
         for (let i = 0; i <= 20; i++) {    
             arr_with_borders[i] = len;
             if (i!=0) sum_arr[i]=sum_arr[i-1]+5000;
             len += one_part_length;
             deb += arr_with_borders[i] + " ";
         }
-        //alert("Left:" + left + " Right:" + right);
-        //alert(deb);
+       
         let new_sum = 0;
         for (let i = 1; i < 20; i++) {
             if (current_pos > arr_with_borders[i - 1] && current_pos < arr_with_borders[i + 1]) { new_sum=sum_arr[i]; break; }
@@ -312,7 +338,6 @@ function dragElement(elmnt) {
 
 function animateValue(id, start, end, duration) {
     // assumes integer values for start and end
-
     var obj = document.getElementById(id);
     var range = end - start;
     // no timer shorter than 50ms (not really visible any way)
@@ -327,7 +352,6 @@ function animateValue(id, start, end, duration) {
     var startTime = new Date().getTime();
     var endTime = startTime + duration;
     var timer;
-
     function run() {
 
   
